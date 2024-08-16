fe09d1b9c843d511daaa008fcfd390fb50c30964
Aron-Balines <balinesaron@gmail.com>
2024-08-16 12:50:27 +0800

    Update DatabaseManager.java

--- src/main/java/com/mycompany/payrollsystem/DatabaseManager.java
+++ src/main/java/com/mycompany/payrollsystem/DatabaseManager.java
@@ -8,30 +8,32 @@ import java.sql.Statement;
 
 public class DatabaseManager {
 
-    public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/payroll_system";
-        String username = "root";
-        String password = "";   
+    private static final String URL = "jdbc:mysql://localhost:3306/payroll_system";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
 
+    public static void main(String[] args) {
+        DatabaseManager dbManager = new DatabaseManager();
+        dbManager.executeQuery("select * from users");
+    }
+
+    public void executeQuery(String query) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection(url, username, password);
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Connection to MySQL database established successfully.");
-            
-            Statement statement = connection.createStatement();
-            
-            ResultSet resultSet = statement.executeQuery("select * from users");
-            
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3));
-            }  
-            
-            connection.close();
-        } 
-        catch (ClassNotFoundException e) {
-            System.err.println("MySQL JDBC Driver not found: " + e.getMessage());
 
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+            }
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found: " + e.getMessage());
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
             System.err.println("SQLState: " + e.getSQLState());
